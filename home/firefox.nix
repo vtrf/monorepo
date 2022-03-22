51d552088606e821ef88336509268d465b04b3cd
Victor Freire <victor@freire.dev.br>
2022-03-22 08:29:07 -0300

    feat(firefox): remove useless stuff

--- home/firefox.nix
+++ home/firefox.nix
@@ -14,6 +14,19 @@ in
         "media.ffmpeg.vaapi.enabled" = true;
         "media.ffvpx.enabled" = true;
         "media.navigator.mediadatadecoder_vpx_enabled" = true;
+
+        # Disable what's new toolbar
+        "browser.messaging-system.whatsNewPanel.enabled" = false;
+        # No Pocket
+        "extensions.pocket.enabled" = false;
+        # No Firefox Sync
+        "identity.fxaccounts.enabled" = false;
+        # No recommended extensions
+        "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons" = false;
+        # No recommended features
+        "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features" = false;
+        # Don't show bookmarks toolbar
+        "browser.toolbars.bookmarks.visibility" = "never";
       };
     };
     extensions = with pkgs.nur.repos.rycee.firefox-addons; [
