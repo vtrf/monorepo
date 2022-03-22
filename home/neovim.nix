88d6c68b3e186a8960bcc275619063ebf77c2e77
Victor Freire <victor@freire.dev.br>
2022-03-21 22:14:11 -0300

    feat(nvim): added todo search

--- home/neovim.nix
+++ home/neovim.nix
@@ -17,45 +17,16 @@ in
         plugin = vimThemeFromScheme { scheme = config.colorscheme; };
         config = "colorscheme nix-${config.colorscheme.slug}";
       }
-      { plugin = telescope-nvim; }
       { plugin = vim-polyglot; }
+
+      { plugin = telescope-nvim; }
+      { plugin = plenary-nvim; }
       {
-        plugin = (nvim-treesitter.withPlugins (plugins:
-        with plugins; [
-        # https://github.com/NixOS/nixpkgs/tree/nixos-unstable/pkgs/development/tools/parsing/tree-sitter/grammars
-        tree-sitter-bash
-        tree-sitter-beancount
-        tree-sitter-bibtex
-        tree-sitter-comment
-        tree-sitter-css
-        tree-sitter-fennel
-        tree-sitter-go
-        tree-sitter-html
-        tree-sitter-javascript
-        tree-sitter-json
-        tree-sitter-latex
-        tree-sitter-lua
-        tree-sitter-make
-        tree-sitter-markdown
-        tree-sitter-nix
-        tree-sitter-python
-        tree-sitter-regex
-        tree-sitter-rust
-        tree-sitter-scss
-        tree-sitter-svelte
-        tree-sitter-toml
-        tree-sitter-typescript
-        tree-sitter-vim
-        tree-sitter-yaml
-      ]));
-      type = "lua";
-      config = ''
-        require 'nvim-treesitter.configs'.setup {
-        highlight = { enable = true },
-        indent = { enable = true },
-        }
-      '';
-    }
+        plugin = pkgs.vimPlugins.todo-comments-nvim;
+        config = ''
+          lua require('todo-comments').setup{}
+        '';
+      }
     ];
 
     # https://github.com/nix-community/home-manager/pull/2391
@@ -76,11 +47,12 @@ in
       " escape key
       inoremap jk <Esc>
 
-      " Find files using Telescope command-line sugar.
+      " Telescope
+      nnoremap <leader>hs <cmd>Telescope help_tags<cr>
       nnoremap <leader>pf <cmd>Telescope find_files<cr>
       nnoremap <leader>ps <cmd>Telescope live_grep<cr>
+      nnoremap <leader>pt <cmd>TodoTelescopex<cr>
       nnoremap <leader>ss <cmd>Telescope buffers<cr>
-      nnoremap <leader>hs <cmd>Telescope help_tags<cr>
     '';
   };
 }
