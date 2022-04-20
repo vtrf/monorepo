cbc2b8021c88c4cb8e8265d646bc2f2df65a7ad2
Victor Freire <victor@freire.dev.br>
2022-04-20 10:01:14 -0300

    email: added new notmuch tags

--- home/email.nix
+++ home/email.nix
@@ -14,9 +14,10 @@ in
       hooks = {
         preNew = "mbsync --all";
         postNew = ''
-          ${notmuch} tag +uber   -- tag:unread and from:noreply@uber.com
-          ${notmuch} tag +nixos  -- tag:unread and from:discourse@discourse.nixos.org
-          ${notmuch} tag +github -- tag:unread and from:notifications@github.com
+          ${notmuch} tag +uber      -- tag:unread and from:noreply@uber.com
+          ${notmuch} tag +nixos     -- tag:unread and from:discourse@discourse.nixos.org
+          ${notmuch} tag +github    -- tag:unread and from:notifications@github.com
+          ${notmuch} tag +sourcehut -- tag:unread and from:*@sr.ht or to:*@lists.sr.ht
         '';
       };
     };
