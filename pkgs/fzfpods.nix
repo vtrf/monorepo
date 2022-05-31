c90d15bdece64854fa65b2f04d219160c9b88f37
Victor Freire <victor@freire.dev.br>
2022-05-31 17:09:44 -0300

    fix(fzfpods): change bash to sh

--- pkgs/fzfpods.nix
+++ pkgs/fzfpods.nix
@@ -13,8 +13,8 @@ writeShellScriptBin "fzfpods" ''
             --prompt "$(${kubectl} config current-context | sed 's/-context$//')> " \
             --header $'/ Enter (${kubectl} exec) / CTRL-O (open log in editor) / CTRL-R (reload) /\n\n' \
             --bind 'ctrl-/:change-preview-window(80%,border-bottom|hidden|)' \
-            --bind 'enter:execute:${kubectl} exec -it --namespace {1} {2} -- bash > /dev/tty' \
-            --bind 'ctrl-o:execute:${EDITOR:-vim} <(${kubectl} logs --all-containers --namespace {1} {2}) > /dev/tty' \
+            --bind 'enter:execute:${kubectl} exec -it --namespace {1} {2} -- sh > /dev/tty' \
+            --bind 'ctrl-o:execute:''${EDITOR:-vim} <(${kubectl} logs --all-containers --namespace {1} {2}) > /dev/tty' \
             --bind 'ctrl-r:reload:$FZF_DEFAULT_COMMAND' \
             --preview-window up:follow \
             --preview '${kubectl} logs --follow --all-containers --tail=10000 --namespace {1} {2}' "$@"
