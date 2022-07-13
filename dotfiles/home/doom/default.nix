58b0a9db2fc4c89b4ea79c367bebe244c0e97ce0
Victor Freire <victor@freire.dev.br>
2022-07-12 22:24:39 -0300

    dotfiles: chore: change emacsPgtkNativeComp to emacs

--- dotfiles/home/doom/default.nix
+++ dotfiles/home/doom/default.nix
@@ -1,13 +1,13 @@
 { pkgs, ... }:
 
 let
-  inherit (pkgs) emacsPgtkNativeComp;
+  inherit (pkgs) emacs;
 in
 {
   programs.doom-emacs = {
     enable = true;
     doomPrivateDir = ./config;
-    emacsPackage = emacsPgtkNativeComp;
+    emacsPackage = emacs;
     emacsPackagesOverlay = final: prev: {
       ts-fold = prev.ts;
       tree-sitter-langs = prev.tree-sitter-langs.override { plugins = pkgs.tree-sitter.allGrammars; };
