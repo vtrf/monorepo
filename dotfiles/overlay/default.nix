97347dfb8cb97f76e461fbafb3ac4ec6f9e1adc2
Victor Freire <victor@freire.dev.br>
2022-07-12 22:22:47 -0300

    dotfiles: refactor: remove hut package overlay

--- dotfiles/overlay/default.nix
+++ dotfiles/overlay/default.nix
@@ -4,5 +4,4 @@ final: prev: {
   # Custom packages
   cli-tools = import ../pkgs/cli-tools.nix { pkgs = final; };
   fzfpods = import ../pkgs/fzfpods.nix { pkgs = prev; };
-  hut = import ../pkgs/hut.nix { pkgs = prev; };
 }
